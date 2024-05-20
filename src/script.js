ab2be0877388b333b28c185eb5eea84599bf7b34
CsengeOnCode <161061835+CsengeOnCode@users.noreply.github.com>
2024-05-20 13:30:16 +0100

    improved the user experience

--- src/script.js
+++ src/script.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a romantic poem about ${userInput.value}...`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
